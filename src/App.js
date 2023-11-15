196a9e4b8e808d99abc1be5756b3f48d07fb9868
nao-nkgw <nao_nkgw@icloud.com>
2023-11-15 20:27:17 +0100

    upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Register from "./pages/user/register";
+import Login from "./pages/user/login"
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/user/register" element={<Register />} />
+        <Route path="/user/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
